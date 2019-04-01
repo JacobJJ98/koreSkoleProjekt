30e21ed2c5896d66c67195b5b671c3762131a5d4
Jacob <jacob-johan@hotmail.com>
2019-04-01 11:34:26 +0200

    Header ændringer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SoegComponent } from './soeg/soeg.component';
-import { KoerelaererComponent } from './soeg/koerelaerer/koerelaerer.component';
-import { KoereskoleComponent } from './soeg/koereskole/koereskole.component';
+import {SoegComponent} from './soeg/soeg.component';
+import {KoerelaererComponent} from './soeg/koerelaerer/koerelaerer.component';
+import {KoereskoleComponent} from './soeg/koereskole/koereskole.component';
 
 @NgModule({
   declarations: [
