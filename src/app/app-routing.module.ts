4f2cd61a9fc7d0d74f09d9a5b88bbfaed6c407e4
Nicolai Dam <nicolaidam96@gmail.com>
2019-04-17 13:11:05 +0200

    auth og struktur ændring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {ForsideComponent} from './forside/forside.component';
-import {KoerelaererComponent} from './forside/soeg/koerelaerer/koerelaerer.component';
-import {OmSideComponent} from './om-side/om-side.component';
+import {AuthGuard} from './auth-guard.service';
+import {LoginSideComponent} from './login-side/login-side.component';
 import {KoerelaererSideComponent} from './koerelaerer-side/koerelaerer-side.component';
-import {LoggedindComponent} from './koerelaerer-side/loggedind/loggedind.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 const appRoutes: Routes = [
-  { path: 'forside', component: ForsideComponent},
-  { path: 'erdukoerelarer', component: KoerelaererSideComponent},
-  { path: 'erdukoerelarer/loggedind', component: LoggedindComponent},
-  { path: 'omos', component: OmSideComponent},
-  { path: '**', redirectTo: '/forside', pathMatch: 'full'}
+  { path: '', component: ForsideComponent},
+
+  { path: 'erdukoerelaerer', component: LoginSideComponent},
+
+  {
+    path: 'koerelaerer',
+     canActivate: [AuthGuard],
+     // canActivateChild: [AuthGuard],
+    component: KoerelaererSideComponent,
+    },
+
+    { path: 'not-found', component: NotFoundComponent, data: {message: 'Page not found!'} },
+    { path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './koerelaerer-side/header/header.component';
 import {SoegComponent} from './forside/soeg/soeg.component';
 import {KoerelaererComponent} from './forside/soeg/koerelaerer/koerelaerer.component';
 import {KoereskoleComponent} from './forside/soeg/koereskole/koereskole.component';
 import { ForsideComponent } from './forside/forside.component';
 import { KoerelaererSideComponent } from './koerelaerer-side/koerelaerer-side.component';
-import { OmSideComponent } from './om-side/om-side.component';
 import {AppRoutingModule} from './app-routing.module';
-import { LoginComponent } from './koerelaerer-side/login/login.component';
-import { LoggedindComponent } from './koerelaerer-side/loggedind/loggedind.component';
 import {HttpModule} from '@angular/http';
+import {AuthService} from './auth.service';
+import {AuthGuard} from './auth-guard.service';
+import { ForsideKoerelaererComponent } from './koerelaerer-side/forside-koerelaerer/forside-koerelaerer.component';
+import { LoginSideComponent } from './login-side/login-side.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
@@ -25,16 +27,16 @@ import {HttpModule} from '@angular/http';
     KoereskoleComponent,
     ForsideComponent,
     KoerelaererSideComponent,
-    OmSideComponent,
-    LoginComponent,
-    LoggedindComponent
+    ForsideKoerelaererComponent,
+    LoginSideComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule
+    HttpModule,
   ],
-  providers: [],
+  providers: [AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
