bc4ff4f94cbdb67602f120229e2a2b16a465c63e
Ahad2906 <s175128@student.dtu.dk>
2019-05-04 20:04:55 +0200

    sortering i app.modle.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './koerelaerer-side/header/header.component';
-import {SoegComponent} from './forside/soeg/soeg.component';
-import {KoerelaererComponent} from './forside/soeg/koerelaerer/koerelaerer.component';
-import {KoereskoleComponent} from './forside/soeg/koereskole/koereskole.component';
 import { ForsideComponent } from './forside/forside.component';
+import { HeaderComponent } from './koerelaerer-side/header/header.component';
+import { KoerelaererComponent } from './forside/soeg/koerelaerer/koerelaerer.component';
+import { KoereskoleComponent } from './forside/soeg/koereskole/koereskole.component';
 import { KoerelaererSideComponent } from './koerelaerer-side/koerelaerer-side.component';
-import {AppRoutingModule} from './app-routing.module';
-import {HttpModule} from '@angular/http';
-import {AuthService} from './auth.service';
-import {AuthGuard} from './auth-guard.service';
 import { LoginSideComponent } from './login-side/login-side.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { VisTilbudSoegComponent } from './forside/vis-tilbud-soeg/vis-tilbud-soeg.component';
-import {TilbudService} from './tilbud.service';
 import { MinetilbudComponent } from './koerelaerer-side/minetilbud/minetilbud.component';
-import { OprettilbudComponent } from './koerelaerer-side/oprettilbud/oprettilbud.component';
 import { MinprofilComponent } from './koerelaerer-side/minprofil/minprofil.component';
 import { MinetilbudStandardComponent } from './koerelaerer-side/minetilbud/minetilbud-standard/minetilbud-standard.component';
 import { MinetilbudListeComponent } from './koerelaerer-side/minetilbud/minetilbud-liste/minetilbud-liste.component';
 import { MinetilbudEditComponent } from './koerelaerer-side/minetilbud/minetilbud-edit/minetilbud-edit.component';
-import {KoreskoleSideService} from './koerelaerer-side/koreskoleSide.service';
 import { MinetilbudItemComponent } from './koerelaerer-side/minetilbud/minetilbud-liste/minetilbud-item/minetilbud-item.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OprettilbudComponent } from './koerelaerer-side/oprettilbud/oprettilbud.component';
+import { SoegComponent } from './forside/soeg/soeg.component';
+import { VisTilbudSoegComponent } from './forside/vis-tilbud-soeg/vis-tilbud-soeg.component';
 
+import { AppRoutingModule } from './app-routing.module';
 
+import { HttpModule } from '@angular/http';
+
+import { AuthService } from './auth.service';
+import { TilbudService } from './tilbud.service';
+import { KoreskoleSideService } from './koerelaerer-side/koreskoleSide.service';
+
+import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ForsideComponent,
     HeaderComponent,
-    SoegComponent,
     KoerelaererComponent,
     KoereskoleComponent,
-    ForsideComponent,
     KoerelaererSideComponent,
     LoginSideComponent,
-    NotFoundComponent,
-    VisTilbudSoegComponent,
     MinetilbudComponent,
-    OprettilbudComponent,
     MinprofilComponent,
     MinetilbudStandardComponent,
     MinetilbudListeComponent,
     MinetilbudEditComponent,
     MinetilbudItemComponent,
+    NotFoundComponent,
+    OprettilbudComponent,
+    SoegComponent,
+    VisTilbudSoegComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     FormsModule,
     HttpModule,
     ReactiveFormsModule
