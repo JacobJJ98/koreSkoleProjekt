c803056ce06e8448be985ce9d3fc7aa256a8704b
Nicolai Dam <nicolaidam96@gmail.com>
2019-05-05 10:20:35 +0200

    brug af Subject til at opdatere listen ved ændring istedet for at hente tilbudene hver gang liste-component loades

--- src/app/koerelaerer-side/minetilbud/minetilbud-liste/minetilbud-liste.component.ts
+++ src/app/koerelaerer-side/minetilbud/minetilbud-liste/minetilbud-liste.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Tilbud} from '../../../Model/tilbud.model';
 import {TilbudService} from '../../../tilbud.service';
 import {TilbudTilBrugere} from '../../../Model/tilbudTilBrugere.model';
 import {KoreskoleSideService} from '../../koreskoleSide.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-minetilbud-liste',
   templateUrl: './minetilbud-liste.component.html',
   styleUrls: ['./minetilbud-liste.component.css']
 })
-export class MinetilbudListeComponent implements OnInit {
+export class MinetilbudListeComponent implements OnInit, OnDestroy {
   tilbud: Tilbud[];
+  subscription: Subscription;
   constructor(private tilbudsservice: KoreskoleSideService) { }
 
   ngOnInit() {
+    this.subscription = this.tilbudsservice.tilbudChanged
+      .subscribe(
+        (tilbud: Tilbud[]) => {
+          this.tilbud = tilbud;
+        }
+      );
     this.tilbud = this.tilbudsservice.henttilbud();
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
