8fc527044f38f1f2f0342aecc7ad3c8fabaa27db
Jacob <jacob-johan@hotmail.com>
2019-05-07 13:03:18 +0200

    Fjernet andet køn

--- src/app/forside/soeg/koerelaerer/koerelaerer.component.ts
+++ src/app/forside/soeg/koerelaerer/koerelaerer.component.ts
@@ -7,7 +7,7 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./koerelaerer.component.css']
 })
 export class KoerelaererComponent implements OnInit, AfterViewInit {
-  konnn = ['Mand', 'Kvinde', 'Andet'];
+  konnn = ['Mand', 'Kvinde'];
   @ViewChild('str') str: ElementRef;
   @ViewChild('køn') køn: ElementRef;
   @ViewChild('f') form: NgForm;
